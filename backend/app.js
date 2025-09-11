436eeff7c67c0b3764f3b4067289c5220a657c64
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2025-09-11 14:46:21 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,14 @@
-// Entry point 
+// Entry point
 
-import express from "express"
-const app = express()
+import express from "express";
+const app = express();
 
-const PORT = 5000
+const PORT = 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log(`server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
 
-
-//
+console.log("hi");
